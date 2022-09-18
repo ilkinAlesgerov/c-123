6f46fa1375aa961bc28ddc919a25cbd253f36dc3
bigdi <bigdi@tak.tak>
2022-09-18 15:24:56 +0300

    dz seminar5

--- Program.cs
+++ Program.cs
@@ -1,79 +1,112 @@
-﻿// Задача 1 
-// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
-// 3, 5 -> 243 (3⁵)
-// 2, 4 -> 16
-// не использую math.pow
-
-Console.WriteLine("Введите ваше 1oe число");
-int number = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите ваше 2oe число");
-int number1 = Convert.ToInt32(Console.ReadLine());
-
-int Vozvedenie(int num, int num2)
-{
-    int k = num2;
-    int vozv = 1;
-    for(int i = 1; i <= k; i++)
-    {
-        vozv *= num;
-    }
-    return vozv;
-} 
-Console.WriteLine(Vozvedenie(number, number1));
-
-//                                              Задача №2
-// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// 452 -> 11
-// 82 -> 10
-// 9012 -> 12
-
-Console.WriteLine("Введите ваше  число");
-int number = Convert.ToInt32(Console.ReadLine());
-
-int DigitSum(int num)
-{
-    int sum = 0;
-    while(num > 0)
-    {
-        sum += num%10;
-        num /= 10;
-    }
-    return sum;
-}
-
-System.Console.WriteLine(DigitSum(number));
-
-//                 задача 3 
-// Напишите программу, которая выводит массив из 8 элементов, заполненный случайными числами.
-// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
-// 1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8 
-//  -> 8
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+//  Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
 
 int[] CreateArray()
 {
+    int[] arr = new int[4];
     Random rnd = new Random();
-    var array = new int[8];
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < arr.Length; i++)
     {
-        array[i] = rnd.Next(0, 100);
-        System.Console.Write($"{array[i]},");
+        arr[i] = rnd.Next(100,1000);
+        System.Console.Write($"{arr[i]} ");
     }
-    System.Console.Write("-> ");
-    return array;
+    return arr;
 }
 
-void ShowArray(int[] array)
+void FindEvenNumbers(int[] array)
 {
+    int countEven = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        
-        Console.Write($"[{array[i]}]");
-        
+        if(array[i]%2==0)
+        {
+            array[i] = 1;
+            countEven += array[i];
+        }
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine(countEven);
+}
+
+FindEvenNumbers(CreateArray());
+
+
+
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] CreateArray()
+{
+    int[] arr = new int[4];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(-10, 100);
+        System.Console.Write($"{arr[i]} ");
+    }
+    return arr;
+}
+
+
+void DigitSum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i%2==1)
+        {
+            sum += array[i];
+        }
+    }
+    System.Console.WriteLine();
+    System.Console.WriteLine(sum);
+}
+
+DigitSum(CreateArray());
+
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] CreateArray()
+{
+    int[] arr = new int[4];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1, 100);
+        System.Console.Write($"{arr[i]} ");
+    }
+    return arr;
+}
+
+void FindDif(int[] array)
+{
+    int max = 0;
+    int min = array[0];
+    var diff = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if (array[i] > max)
+        {
+            max = array[i];
+
+        }
+        if (array[i] < min)
+        {
+            min = array[i];
+
+        }
     }
     
+    System.Console.WriteLine();
+    System.Console.WriteLine($"{max} , {min}");
+    diff = max - min;
+    System.Console.WriteLine(diff);
 }
 
-
-ShowArray(CreateArray());
-
-
+FindDif(CreateArray());
