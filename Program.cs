a9c22cf6d91c39ebf23fdf610e8cea10415d88ac
bigdi <bigdi@tak.tak>
2022-09-21 21:36:42 +0300

    dz sem6

--- Program.cs
+++ Program.cs
@@ -72,7 +72,6 @@ double[] DigitCrossPoint()
 
     crossPoint[0] = (points[1,0] - points[0, 0]) / (points[0, 1] - points[1, 1]);
     crossPoint[1] = (points[0, 1] * crossPoint[0]) + points[0, 0];
-    System.Console.WriteLine(points[1,1]-points[0,1]);
     return crossPoint;
 }
 
